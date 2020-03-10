f4c7d2c798ad5ec40121158eedee200a11fa1408
fangzhiyang <fanmcgradfy@gmail.com>
2020-03-10 20:31:34 +0800

    修改

--- src/test/test.py
+++ src/test/test.py
@@ -1,22 +1,5 @@
-import sys
-import traceback
+import lief
 
-import numpy
-
-sys.path.append('../novel_feature')
-from feature_extraction import *
-
-directory_name = os.path.join('../../Dataset', 'train') + '/'
-files = os.listdir(directory_name)
-files = numpy.sort(files)
-byte_files = [i for i in files if i.endswith('.bytes')]
-
-for t, fname in enumerate(byte_files):
-    with open(directory_name + fname, 'r') as f:
-        try:
-            entropy = byte_entropy(f)
-            if (t + 1) % 100 == 0:
-                print("processing", t)
-        except Exception as err:
-            print(err, traceback.print_exc())
-            print("Error", fname)
+pe = '../../Dataset/pe/backdoor/Backdoor.Win32.Rbot.pzu'
+binary = lief.PE.parse(pe)
+print(binary)
