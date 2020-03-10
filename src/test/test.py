a36c16d67b049df041c9fbab18bf27571bc1c6c3
fangzhiyang <fanmcgradfy@gmail.com>
2020-03-10 21:01:50 +0800

    修改

--- src/test/test.py
+++ src/test/test.py
@@ -1,14 +1,15 @@
 import lief
 
+from gym_malware.envs.controls.manipulate2 import MalwareManipulator
+
 pe = '../../Dataset/pe/backdoor/Backdoor.Win32.Rbot.pzu'
+binary = lief.PE.parse(pe)
 
-
-def fetch_file():
-    with open(pe, 'rb') as infile:
-        bytez = infile.read()
-
-    return bytez
-
-
-binary = lief.PE.parse(fetch_file())
-print(binary)
+def test_overlay_append():
+    with open(pe, 'rb') as f:
+        bytez = f.read()
+    bytez2 = MalwareManipulator(pe).ARBE()
+    if len(bytez) == len(bytez2):
+        return 0
+    else:
+        return 1
