f84c85c7789e5310db2f1a65dc815f0952317499
fangzhiyang <fanmcgradfy@gmail.com>
2020-03-14 20:24:45 +0800

    修改

---
+++ src/test/1.py
@@ -0,0 +1,5 @@
+
+str = 'backdoor.222.333'
+f = str.split('.')[0].capitalize()
+print(f)
+print(str)
--- src/test/test.py
+++ src/test/test.py
@@ -21,15 +21,13 @@ with open(label_file, 'r') as csv:
 del_pe_count = 0
 del_bytes_count = 0
 del_label_count = 0
-for filename, tuple in mc_map.items():
+for filename in mc_map.keys():
     # 删除pe目录样本
-    fold_list = os.listdir(pe)
-    for i, fold in enumerate(fold_list):
-        files = os.listdir(os.path.join(pe, fold))
-        for f in files:
-            if f == filename:
-                os.remove(os.path.join(os.path.join(pe, fold), f))
-                del_pe_count += 1
+    fold = filename.split('.')[0].capitalize()
+    pp = os.path.join(os.path.join(pe, fold), filename)
+    if os.path.exists(pp):
+        os.remove(pp)
+        del_pe_count += 1
 
     # 删除.bytes
     ff = os.path.join(train, filename + '.bytes')
@@ -39,31 +37,16 @@ for filename, tuple in mc_map.items():
 
     # 重新构造trainLabel.csv
     for cc in content:
-        if filename + '.bytes' in cc:
+        if filename in cc:
             content_new.remove(cc)
             del_label_count += 1
 
-print("原有{}个pe样本，删除{}个".format(len(content) - 1, del_pe_count))
-print("删除{}个bytes文件".format(del_bytes_count))
+bytes = glob.glob(os.path.join(train, '*.bytes'))
+
+print("原有{}个pe样本，删除{}个，剩余{}个".format(len(content) - 1, del_pe_count, len(content) - 1 - del_pe_count))
+print("删除{}个bytes文件，剩余{}个".format(del_bytes_count, len(bytes)))
 print("原有{}个label，删除{}个label，content_new剩余{}个"
       .format(len(content) - 1, del_label_count, len(content_new) - 1))
-
-re_del_bytes_count = 0
-# 再次清理bytes
-bytes = os.listdir(train)
-tqdm_bytes = tqdm(bytes)
-for bb in tqdm_bytes:
-    tqdm_bytes.set_description(bb)
-    del_flag = True
-    for ll in content_new:
-        if bb.replace('.bytes', '') in ll:
-            del_flag = False
-            break
-    if del_flag:
-        os.remove(os.path.join(train, bb))
-        re_del_bytes_count += 1
-
-print("重新清理{}个bytes文件，剩余{}个".format(re_del_bytes_count, len(glob.glob(os.path.join(train, "*.bytes")))))
 
 with open(label_file, 'w') as csv:
     csv.writelines(content_new)
