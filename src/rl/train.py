2f086e14ee3436214d679861a6048e4bafac4803
fangzhiyang <fanmcgradfy@gmail.com>
2020-03-26 00:13:49 +0800

    完善rl过程

--- src/rl/train.py
+++ src/rl/train.py
@@ -202,7 +202,7 @@ def main():
             eval_n_episodes=None,
             outdir=args.outdir,  # Save everything to 'result' directory
             step_hooks=[step_q_hook, step_loss_hook],
-            successful_score=5,
+            successful_score=7,
             eval_env=test_env
         )
 
